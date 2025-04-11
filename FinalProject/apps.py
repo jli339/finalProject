7ca3fa7e4b30560988245353bfa58d2aaadade6c
JiahaoLi <838124877@qq.com>
2025-04-11 19:22:33 +0800

    quick update

--- FinalProject/apps.py
+++ FinalProject/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FinalprojectConfig(AppConfig):
+class FinalProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'FinalProject'
